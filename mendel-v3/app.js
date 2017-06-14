384858c1a0013a86dc4daa869ed76081a7f3b25d
Shalom Volchok <shalom@digitaloptgroup.com>
2017-06-14 14:12:14 +0300

    removed stray .git from v3 example

---
+++ mendel-v3/app.js
@@ -0,0 +1,131 @@
+/* Copyright 2015, Yahoo Inc.
+   Copyrights licensed under the MIT License.
+   See the accompanying LICENSE file for terms. */
+var ReactDOMServer = require("react-dom/server");
+var express = require("express");
+var logger = require("morgan");
+var MendelMiddleware = require("mendel-middleware");
+var fs = require("fs");
+var cache = true;
+const compression = require("compression");
+
+if (process.env.NODE_ENV !== "production") {
+    MendelMiddleware = require("mendel-development-middleware");
+    cache = false;
+}
+
+if (String(process.env.MENDEL_CACHE) === "false") {
+    cache = false;
+}
+
+var app = express();
+app.use(compression());
+
+app.use(logger("tiny"));
+app.use(MendelMiddleware());
+app.set("query parser", "simple");
+
+app.get("/", function(req, res) {
+    var variations = (req.query.variations || "")
+        .trim()
+        .split(",")
+        .filter(Boolean);
+    var serverRender = req.query.ssr !== "false" && req.mendel.isSsrReady();
+    var optionalMarkup = "";
+
+    // populates req.mendel.variations with validated and sorted variations
+    req.mendel.setVariations(variations);
+
+    // if (serverRender) {
+    //     // To improve ssr performance, you need to pass
+    //     // array of bundle ids you only need for ssr rendering
+    //     const resolver = req.mendel.resolver(["main"]);
+    //     var Main = resolver.require("./main");
+    //     optionalMarkup = ReactDOMServer.renderToString(Main());
+    // }
+
+    var html = [
+        "<!DOCTYPE html>",
+        "<head>",
+        "<script>var global = global || window;</script>",
+        "</head>",
+        "<body>",
+        '<div id="main">' + optionalMarkup + "</div>",
+        bundle(req, "main"),
+        // The full example supports on-demand loading, lazy bundle
+        // is only loaded client-side when a button is clicked in the
+        // application
+        "</body>"
+    ].join("\n");
+
+    res.send(html);
+    res.end();
+});
+
+// Simple caching added to example, since very large applications can experience
+// overhead added by mendel hashing bundles.
+// Caching is not part of the middleware itself because large deployments with
+// dozens or hundreds of nodejs processes will prefer distributed caches such as
+// memcached, redis, etc
+var bundleCache = {};
+var entryMapCache = {};
+
+function bundle(req, bundle) {
+    var key = bundle + ":" + req.mendel.variations.join(":");
+    if (!cache || !bundleCache[key]) {
+        if (bundle === "css") {
+            if (process.env.NODE_ENV !== "production") {
+                bundleCache[key] =
+                    '<link rel="stylesheet" type="text/css" href="' +
+                    req.mendel.getURL(bundle) +
+                    '">'; // eslint-disable-line max-len
+            } else {
+                bundleCache[key] =
+                    "<style>" +
+                    // HACK remove hardcode by tabulating resource map
+                    fs.readFileSync("./build/main.css", "utf8") +
+                    "</style>";
+            }
+        } else {
+            bundleCache[key] =
+                '<script src="' + req.mendel.getURL(bundle) + '"></script>';
+        }
+    }
+    return bundleCache[key];
+}
+
+function entryMap(req, bundle) {
+    var key = bundle + ":" + req.mendel.variations.join(":");
+    if (!cache || !entryMapCache[key]) {
+        // `req.mendel.getBundleEntries` contains all bundles as keys and arrays
+        // of entries that were used (normalized by variations) as values. This
+        // allows apps to create a specific logic with their bundles in the
+        // runtime.
+        var bundles = req.mendel.getBundleEntries(bundle);
+
+        // In this particular case, entryMap will be used to expose to the client
+        // the URL for bundles based on modules that are "exposed", meaning, after
+        // loading the bundle, you can `require('entryName.js')` for any entry.
+        var entryMapScript = [
+            "<script>",
+            "   (function(){",
+            '       var nameSpace = "_mendelEntryMap";',
+            '       var url = "' + req.mendel.getURL(bundle) + '";',
+            "       window[nameSpace] = window[nameSpace] || {};",
+            "       " +
+                bundles
+                    .map(function(entry) {
+                        return (
+                            'window[nameSpace]["' + entry + '"] = ' + " url;"
+                        );
+                    })
+                    .join("\n       "),
+            "   })()",
+            "</script>"
+        ];
+        entryMapCache[key] = entryMapScript.join("\n");
+    }
+    return entryMapCache[key];
+}
+
+module.exports = app;
